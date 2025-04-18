20d9600f7e864941cdaa1386d8fab9075d6aa4c3
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-04-18 17:25:50 +0800

    feat: 完成无限隧道穿梭

---
+++ infinite-tunnel/src/main.js
@@ -0,0 +1,35 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import mesh from "./mesh.js";
+
+const scene = new THREE.Scene();
+
+scene.add(mesh);
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const camera = new THREE.PerspectiveCamera(60, width / height, 1, 1000);
+camera.position.set(0.9, -520, 6.5);
+camera.lookAt(0, 0, 0);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+let H = 0;
+function render() {
+  H += 0.002;
+  if (H > 1) {
+    H = 0;
+  }
+  mesh.material.color.setHSL(H, 0.5, 0.5);
+  mesh.material.alphaMap.offset.y += 0.01;
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+render();
+
+document.body.append(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
